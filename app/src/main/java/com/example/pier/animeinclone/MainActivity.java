b92fbf4ed7423a02e25d6e847365c62615ebd2d5
PIER-PC\PIER <piersrv@gmail.com>
2018-07-17 20:00:56 +0700

    clean 4

--- app/src/main/java/com/example/pier/animeinclone/MainActivity.java
+++ app/src/main/java/com/example/pier/animeinclone/MainActivity.java
@@ -44,12 +44,6 @@ public class MainActivity extends AppCompatActivity implements
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
         loadFragment(HomeFragment.newInstance("",""));
-        // initSlider();
-
-      //  recycleOne();
-      //  recycleTwo();
-      //  recycleThree();
-      //  recycleFour();
     }
 
     private void loadFragment(Fragment fragment) {
@@ -57,31 +51,6 @@ public class MainActivity extends AppCompatActivity implements
         transaction.replace(R.id.frame_container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
-    }
-
-    private void initSlider() {
-        sliderShow = findViewById(R.id.slider);
-
-        HashMap<String,String> url_maps = new HashMap<>();
-        url_maps.put("Sword Art Online", "https://ae01.alicdn.com/kf/HTB16Xa3jCfD8KJjSszhq6zIJFXaw/DIY-frame-Sword-Art-Online-Light-Novel-Japan-Hot-Comics-Anime-Poster-Landscape-Fabric-Silk-Posters.jpg_640x640.jpg");
-        url_maps.put("Bleach", "https://ae01.alicdn.com/kf/HTB1KwuvjBDH8KJjSspnq6zNAVXac/DIY-frame-Bleach-Japan-Classic-Comics-Anime-Poster-Moon-Landscape-Fabric-Silk-Posters-And-Prints-For.jpg_640x640.jpg");
-
-        for(String name : url_maps.keySet()){
-            TextSliderView textSliderView = new TextSliderView(this);
-            // initialize a SliderLayout
-            textSliderView
-                    .description(name)
-                    .image(url_maps.get(name))
-                    .setScaleType(BaseSliderView.ScaleType.Fit);
-            //    .setOnSliderClickListener(this);
-
-            //add your extra information
-            //   textSliderView.bundle(new Bundle());
-            //   textSliderView.getBundle()
-            //          .putString("extra",name);
-
-            sliderShow.addSlider(textSliderView);
-        }
     }
 
     @Override
