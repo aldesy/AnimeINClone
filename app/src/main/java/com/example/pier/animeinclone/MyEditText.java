47efd4213716f8c8f92b8454c6b63a990937ad7a
PIER-PC\PIER <piersrv@gmail.com>
2018-07-21 20:12:36 +0700

    clean

--- app/src/main/java/com/example/pier/animeinclone/MyEditText.java
+++ app/src/main/java/com/example/pier/animeinclone/MyEditText.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Toast;
 
 public class MyEditText extends AppCompatEditText{
     private Context mContext;
